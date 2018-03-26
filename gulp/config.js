93a7b673d2ae0a32ef3e8dcadff1bc18dec829dc
kazuo1234 <dev@seacrea.jp>
2018-03-26 10:49:30 +0000

    kadai

---
+++ gulp/config.js
@@ -0,0 +1,11 @@
+module.exports ={
+    source: {
+        sass: './src/css/',
+        js:   './src/js/'
+    },
+    dest: {
+        html: './src',
+        css:  './src/css/',
+        js:   './src/js/'
+    }
+};
